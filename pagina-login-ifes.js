706ab61681f9f2713cc7ecaee8dbce1aa64669ad
zarielbloomp <155508551+zarielbloomp@users.noreply.github.com>
2024-02-20 12:15:10 -0300

    fiz no ifes de teste no dia 20/02/24

---
+++ pagina-login-ifes.js
@@ -0,0 +1,8 @@
+function validarFormulario(){
+    let name = document.getElementById('iname').value;
+    let email = document.getElementById('iemail').value;
+    let password = document.getElementById('ipassword').value;
+
+    let 
+
+}
